4198531c709eacf4475917e0bd810f4728265685
Jugal Kishor <jugal.kishor@startbitsolutions.com>
2019-04-05 10:27:02 +0530

    Signed-off-by: Jugal Kishor <jugal.kishor@startbitsolutions.com>

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname));
 //To allow cross origin request
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://facing-forward.org');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,12 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
-
+router.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 var eventLogCtrl = require('../controller/eventLog.controller');
 
 router.post('/eventLog/create', eventLogCtrl.create);
