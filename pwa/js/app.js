e72a12b294bcdb2aa35cc18d99af8ceb0764bb0e
Jugal Kishor <jugal.kishor@startbitsolutions.com>
2019-04-10 17:17:49 +0530

    Signed-off-by: Jugal Kishor <jugal.kishor@startbitsolutions.com>

--- pwa/js/app.js
+++ pwa/js/app.js
@@ -1,15 +1,18 @@
 (function() {
     'use strict';
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-137800914-1']);
-    _gaq.push(['_trackPageview']);
-
     var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
+    $(document).ready(function() {
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-137800914-1']);
+        _gaq.push(['_trackPageview']);
+
+
+        ga.type = 'text/javascript';
+        ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+    });
 
     function isMobileSafari() {
         return false;
